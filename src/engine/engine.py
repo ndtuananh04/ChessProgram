973e4c2f3cd096d46efe30272bb1c5a2c2e1232a
ndtuananh04 <ndtuananh04@gmail.com>
2025-05-16 07:32:20 +0700

    minor

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -5,56 +5,56 @@ import zlib
 from itertools import count
 from collections import namedtuple, defaultdict
 
-piece = {"P": 100, "N": 280, "B": 320, "R": 479, "Q": 929, "K": 60000}
+piece = {"P": 100, "N": 280, "B": 320, "R": 480, "Q": 930, "K": 60000}
 pst = {
     'P': (   0,   0,   0,   0,   0,   0,   0,   0,
-            78,  83,  86,  73, 102,  82,  85,  90,
-             7,  29,  21,  44,  40,  31,  44,   7,
-           -17,  16,  -2,  15,  14,   0,  15, -13,
-           -26,   3,  10,   9,   6,   1,   0, -23,
-           -22,   9,   5, -11, -10,  -2,   3, -19,
-           -31,   8,  -7, -37, -36, -14,   3, -31,
+            80,  85,  85,  85,  85,  85,  85,  80,
+            10,  30,  35,  40,  40,  35,  30,  10,
+           -15,  15,   0,  15,  15,   0,  15, -15,
+           -25,   5,  10,  10,  10,  10,   5, -25,
+           -20,   5,   5,  -5,  -5,   5,   5, -20,
+           -30,   5,  -5, -35, -35,  -5,   5, -30,
              0,   0,   0,   0,   0,   0,   0,   0),
-    'N': ( -66, -53, -75, -75, -10, -55, -58, -70,
-            -3,  -6, 100, -36,   4,  62,  -4, -14,
-            10,  67,   1,  74,  73,  27,  62,  -2,
-            24,  24,  45,  37,  33,  41,  25,  17,
-            -1,   5,  31,  21,  22,  35,   2,   0,
-           -18,  10,  13,  22,  18,  15,  11, -14,
-           -23, -15,   2,   0,   2,   0, -23, -20,
-           -74, -23, -26, -24, -19, -35, -22, -69),
-    'B': ( -59, -78, -82, -76, -23,-107, -37, -50,
-           -11,  20,  35, -42, -39,  31,   2, -22,
-            -9,  39, -32,  41,  52, -10,  28, -14,
-            25,  17,  20,  34,  26,  25,  15,  10,
-            13,  10,  17,  23,  17,  16,   0,   7,
-            14,  25,  24,  15,   8,  25,  20,  15,
-            19,  20,  11,   6,   7,   6,  20,  16,
-            -7,   2, -15, -12, -14, -15, -10, -10),
-    'R': (  35,  29,  33,   4,  37,  33,  56,  50,
-            55,  29,  56,  67,  55,  62,  34,  60,
-            19,  35,  28,  33,  45,  27,  25,  15,
-             0,   5,  16,  13,  18,  -4,  -9,  -6,
-           -28, -35, -16, -21, -13, -29, -46, -30,
-           -42, -28, -42, -25, -25, -35, -26, -46,
-           -53, -38, -31, -26, -29, -43, -44, -53,
-           -30, -24, -18,   5,  -2, -18, -31, -32),
-    'Q': (   6,   1,  -8,-104,  69,  24,  88,  26,
-            14,  32,  60, -10,  20,  76,  57,  24,
-            -2,  43,  32,  60,  72,  63,  43,   2,
-             1, -16,  22,  17,  25,  20, -13,  -6,
-           -14, -15,  -2,  -5,  -1, -10, -20, -22,
-           -30,  -6, -13, -11, -16, -11, -16, -27,
-           -36, -18,   0, -19, -15, -15, -21, -38,
-           -39, -30, -31, -13, -31, -36, -34, -42),
-    'K': (   4,  54,  47, -99, -99,  60,  83, -62,
-           -32,  10,  55,  56,  56,  55,  10,   3,
-           -62,  12, -57,  44, -67,  28,  37, -31,
-           -55,  50,  11,  -4, -19,  13,   0, -49,
-           -55, -43, -52, -28, -51, -47,  -8, -50,
-           -47, -42, -43, -79, -64, -32, -29, -32,
-            -4,   3, -14, -50, -57, -18,  13,   4,
-            17,  30,  -3, -14,   6,  -1,  40,  18),
+    'N': ( -65, -55, -55, -10, -10, -55, -55, -65,
+            -5,  -5,  60,   0,   0,  60,  -5,  -5,
+            10,  65,  15,  75,  75,  15,  65,  10,
+            20,  25,  40,  35,  35,  40,  25,  20,
+             0,   5,  30,  20,  20,  30,   5,   0,
+           -15,  10,  15,  20,  20,  15,  10, -15,
+           -20, -15,   0,   0,   0,   0, -15, -20,
+           -70, -25, -25, -20, -20, -25, -25, -70),
+    'B': ( -50, -40, -60, -25, -25, -60, -40, -50,
+           -15,  15,  35, -40, -40,  35,  15, -15,
+           -10,  30, -15,  45,  45, -15,  30, -10,
+            20,  15,  25,  30,  30,  25,  15,  20,
+            10,  15,  15,  20,  20,  15,  15,  10,
+            15,  20,  20,  10,  10,  20,  20,  15,
+            15,  20,  10,   5,   5,  10,  20,  15,
+           -10,   0, -10, -15, -15, -10,   0, -10),
+    'R': (  40,  30,  35,  35,  35,  35,  30,  40,
+            55,  30,  55,  60,  60,  55,  30,  55,
+            20,  30,  30,  40,  40,  30,  30,  20,
+             0,   5,  15,  15,  15,  15,   5,   0,
+           -30, -25, -15, -15, -15, -15, -25, -30,
+           -40, -30, -35, -25, -25, -35, -30, -40,
+           -50, -40, -35, -30, -30, -35, -40, -50,
+           -30, -25, -20,   0,   0, -20, -25, -30),
+    'Q': (  15,  10,  20,  25,  25,  20,  10,  15,
+            15,  30,  60,  65,  65,  60,  30,  15,
+             0,  40,  35,  65,  65,  35,  40,   0,
+             0, -15,  20,  20,  20,  20, -15,   0,
+           -15, -15,  -5,  -5,  -5,  -5, -15, -15,
+           -25, -10, -15, -15, -15, -15, -10, -25,
+           -35, -20, -15, -15, -15, -15, -20, -35,
+           -40, -35, -30, -30, -30, -30, -35, -40),
+    'K': (  10,  50,  50, -80, -80,  50,  50,  10,
+           -15,  10,  55,  55,  55,  55,  10, -15,
+           -45,  15, -25,  35,  35, -25,  15, -45,
+           -50,  10,   5,  -5,  -5,   5,  10, -50,
+           -50, -45, -50, -30, -30, -50, -45, -50,
+           -40, -35, -40, -65, -65, -40, -35, -40,
+             0,   5, -15, -50, -50, -15,   5,   0,
+            15,  25,   0, -10, -10,   0,  25,  15),
 }
 # Pad tables and join piece and pst dictionaries
 for k, table in pst.items():
@@ -92,15 +92,9 @@ directions = {
     "K": (N, E, S, W, N+E, S+E, S+W, N+W)
 }
 
-# Mate value must be greater than 8*queen + 2*(rook+knight+bishop)
-# King value is set to twice this value such that if the opponent is
-# 8 queens up, but we got the king, we still exceed MATE_VALUE.
-# When a MATE is detected, we'll set the score to MATE_UPPER - plies to get there
-# E.g. Mate in 3 will be MATE_UPPER - 6
 MATE_LOWER = piece["K"] - 10 * piece["Q"]
 MATE_UPPER = piece["K"] + 10 * piece["Q"]
 
-# Constants for tuning search
 QS = 40
 QS_A = 140
 EVAL_ROUGHNESS = 15
@@ -133,9 +127,6 @@ class Position(namedtuple("Position", "board score wc bc ep kp")):
     """
 
     def gen_moves(self):
-        # For each of our pieces, iterate through each possible 'ray' of moves,
-        # as defined in the 'directions' map. The rays are broken e.g. by
-        # captures or immediately in case of pieces such as knights.
         for i, p in enumerate(self.board):
             if not p.isupper():
                 continue
@@ -312,44 +303,19 @@ class Searcher:
             if gamma <= s* then gamma <= r <= s* (A better lower bound) """
         self.nodes += 1
         
-
-        # Depth <= 0 is QSearch. Here any position is searched as deeply as is needed for
-        # calmness, and from this point on there is no difference in behaviour depending on
-        # depth, so so there is no reason to keep different depths in the transposition table.
         depth = max(depth, 0)
 
-        # Sunfish is a king-capture engine, so we should always check if we
-        # still have a king. Notice since this is the only termination check,
-        # the remaining code has to be comfortable with being mated, stalemated
-        # or able to capture the opponent king.
         if pos.score <= -MATE_LOWER:
             return -MATE_UPPER
 
-        # Look in the table if we have already searched this position before.
-        # We also need to be sure, that the stored search was over the same
-        # nodes as the current search.
         entry = self.tp_score.get((pos, depth, can_null), Entry(-MATE_UPPER, MATE_UPPER))
         if entry.lower >= gamma: return entry.lower
         if entry.upper < gamma: return entry.upper
 
-        # Let's not repeat positions. We don't chat
-        # - at the root (can_null=False) since it is in history, but not a draw.
-        # - at depth=0, since it would be expensive and break "futulity pruning".
         if can_null and depth > 0 and pos in self.history:
             return 0
 
-        # Generator of moves to search in order.
-        # This allows us to define the moves, but only calculate them if needed.
         def moves():
-            # First try not moving at all. We only do this if there is at least one major
-            # piece left on the board, since otherwise zugzwangs are too dangerous.
-            # FIXME: We also can't null move if we can capture the opponent king.
-            # Since if we do, we won't spot illegal moves that could lead to stalemate.
-            # For now we just solve this by not using null-move in very unbalanced positions.
-            # TODO: We could actually use null-move in QS as well. Not sure it would be very useful.
-            # But still.... We just have to move stand-pat to be before null-move.
-            #if depth > 2 and can_null and any(c in pos.board for c in "RBNQ"):
-            #if depth > 2 and can_null and any(c in pos.board for c in "RBNQ") and abs(pos.score) < 500:
             if depth > 2 and can_null and abs(pos.score) < 500:
                 yield None, -self.bound(pos.rotate(nullmove=True), 1 - gamma, depth - 3)
 
@@ -372,10 +338,6 @@ class Searcher:
             # promotions). Otherwise we do all moves. This is called quiescent search.
             val_lower = QS - depth * QS_A
 
-            # Only play the move if it would be included at the current val-limit,
-            # since otherwise we'd get search instability.
-            # We will search it again in the main loop below, but the tp will fix
-            # things for us.
             if killer and pos.value(killer) >= val_lower:
                 yield killer, -self.bound(pos.move(killer), 1 - gamma, depth - 1)
 
@@ -408,24 +370,6 @@ class Searcher:
                     self.tp_move[pos] = move
                 break
 
-        # Stalemate checking is a bit tricky: Say we failed low, because
-        # we can't (legally) move and so the (real) score is -infty.
-        # At the next depth we are allowed to just return r, -infty <= r < gamma,
-        # which is normally fine.
-        # However, what if gamma = -10 and we don't have any legal moves?
-        # Then the score is actaully a draw and we should fail high!
-        # Thus, if best < gamma and best < 0 we need to double check what we are doing.
-
-        # We will fix this problem another way: We add the requirement to bound, that
-        # it always returns MATE_UPPER if the king is capturable. Even if another move
-        # was also sufficient to go above gamma. If we see this value we know we are either
-        # mate, or stalemate. It then suffices to check whether we're in check.
-
-        # Note that at low depths, this may not actually be true, since maybe we just pruned
-        # all the legal moves. So sunfish may report "mate", but then after more search
-        # realize it's not a mate after all. That's fair.
-
-        # This is too expensive to test at depth == 0
         if depth > 2 and best == -MATE_UPPER:
             flipped = pos.rotate(nullmove=True)
             # Hopefully this is already in the TT because of null-move
@@ -653,52 +597,3 @@ def render(i):
 
 hist = [Position(initial, 0, (True, True), (True, True), 0, 0)]
 
-#input = raw_input
-
-# minifier-hide start
-# import sys, tools.uci
-# tools.uci.run(sys.modules[__name__], hist[-1])
-# sys.exit()
-# minifier-hide end
-
-# if __name__ == "__main__":
-#     hist = [Position(initial, 0, (True, True), (True, True), 0, 0)]
-
-#     searcher = Searcher()
-#     while True:
-#         args = input().split()
-#         if args[0] == "isready":
-#             print("readyok")
-
-#         elif args[0] == "quit":
-#             break
-
-#         elif args[:2] == ["position", "startpos"]:
-#             del hist[1:]
-#             for ply, move in enumerate(args[3:]):
-#                 i, j, prom = parse(move[:2]), parse(move[2:4]), move[4:].upper()
-#                 if ply % 2 == 1:
-#                     i, j = 119 - i, 119 - j
-#                 hist.append(hist[-1].move(Move(i, j, prom)))
-
-#         elif args[0] == "go":
-#             wtime, btime, winc, binc = [int(a) / 1000 for a in args[2::2]]
-#             if len(hist) % 2 == 0:
-#                 wtime, winc = btime, binc
-#             think = min(wtime / 40 + winc, wtime / 2 - 1)
-
-#             start = time.time()
-#             move_str = None
-#             for depth, gamma, score, move in Searcher().search(hist):
-#                 # The only way we can be sure to have the real move in tp_move,
-#                 # is if we have just failed high.
-#                 if score >= gamma:
-#                     i, j = move.i, move.j
-#                     if len(hist) % 2 == 0:
-#                         i, j = 119 - i, 119 - j
-#                     move_str = render(i) + render(j) + move.prom.lower()
-#                     print("info depth", depth, "score cp", score, "pv", move_str)
-#                 if move_str and time.time() - start > think * 0.8:
-#                     break
-
-#             print("bestmove", move_str or '(none)')
